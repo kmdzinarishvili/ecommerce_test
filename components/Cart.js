6154df55fb46702a89e9ca11bec81c6d3f8003f6
keti <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-03-15 19:01:27 +0400

    got rid of shadow

--- components/Cart.js
+++ components/Cart.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
+import { Image } from 'react-native';
 
 const Cart = () => {
     const image = { uri: 'https://media.istockphoto.com/vectors/shopping-cart-icon-isolated-on-white-background-vector-id1206806317?k=6&m=1206806317&s=612x612&w=0&h=Fo7D7nh_QPu758KRdbNTp7m4xSVOxBvJ2cfUvA1_k_U=' }
     return (
-        <ImageBackground
-            imageStyle={{
-                borderRadius: 25
-            }
-            }
+        <Image
             source={image}
             style={{
                 width: 50,
                 height: 50,
                 borderRadius: 25,
-                backgroundColor: 'pink',
+                borderWidth:10,
                 position: 'absolute',
                 bottom: 100,
                 right: 50,
                 shadowColor: '#000',
-                zIndex: -1,
-                shadowOffset: {
-                    width: 6,
-                    height: 6,
-                },
-                shadowOpacity: 1,
-                shadowRadius: 15,
-                elevation: 1,
+                zIndex: 100,
+                // shadowOffset: {
+                //     width: 6,
+                //     height: 6,
+                // },
+                // shadowOpacity: 1,
+                // shadowRadius: 15,
             }}
         />);
 }
